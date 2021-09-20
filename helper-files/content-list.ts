4608da65009619213953818ac0cc6d860618b692
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-09-20 14:06:56 -0400

    Typescript - Generate component, pipe, and directive script.

--- helper-files/content-list.ts
+++ helper-files/content-list.ts
@@ -4,18 +4,28 @@ export class ContentList {
   static contentCount = 0;
   _items: Content[];
 
+  //Constructor
   constructor(item: Content) {
     this._items = []; // initialize array
     this._items[0] = item;
     this.increaseCount();
   }
+
+  //getter function returns Content array
   get items(): Content[]{
     return this._items;
   }
   set items(newItems: Content[]){
     this._items = newItems;
   }
+
+  //add function that adds 1 Content to end of array
+  //function returns number of items in array
+
+  //function that takes input of an index and returns reader-friendly html
+  //output of a Content items properties
   increaseCount() {
     return ++ContentList.contentCount;
   }
+
 }
