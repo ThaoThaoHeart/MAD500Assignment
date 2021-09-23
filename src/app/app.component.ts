c7e275b102378de496c8ef7e3235fa1b5ddd637f
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-09-22 21:57:05 -0400

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Content} from "./helper-files/content-interface";
 
 @Component({
   selector: 'app-root',
@@ -10,55 +9,9 @@ import {Content} from "./helper-files/content-interface";
 export class AppComponent {
   title = 'angular';
   static contentCount = 0;
-  contentList: Content[];
+
 
 constructor() {
-  this.contentList = [{
-    id: 0,
-    author: "Person",
-    imgUrl: "",
-    type: "",
-    title: "A Title",
-    body: "The Body",
-    tags: []
-  },
-    {
-      id: 1,
-      author: "Person 1",
-      imgUrl: "",
-      type: "",
-      title: "A Title 1",
-      body: "The Body 1",
-      tags: []
-    },
-    {
-      id: 2,
-      author: "Person 2",
-      imgUrl: "",
-      type: "",
-      title: "A Title 2",
-      body: "The Body 2",
-      tags: []
-    },
-    {
-      id: 3,
-      author: "Person 3",
-      imgUrl: "",
-      type: "",
-      title: "A Title 3",
-      body: "The Body 3",
-      tags: []
-    },
-    {
-      id: 4,
-      author: "Person 4",
-      imgUrl: "",
-      type: "",
-      title: "A Title 4",
-      body: "The Body 4",
-      tags: []
-    }
-  ]
 }
 
   //When Instantiate, structure called.
