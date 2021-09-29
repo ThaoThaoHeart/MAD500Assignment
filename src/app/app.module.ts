ce8bc717743bfe62f91fbc0b090a812f976a549b
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-09-29 11:52:34 -0400

    Create Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { FilterPipePipe } from './filter-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
-    ContentListComponent
+    ContentListComponent,
+    FilterPipePipe
   ],
   imports: [
     BrowserModule,
