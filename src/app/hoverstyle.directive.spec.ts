4b74763c530eabce078b7d7af5042d7f91d1bc42
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-10-06 12:42:33 -0400

    Hover directive working

---
+++ src/app/hoverstyle.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HoverstyleDirective } from './hoverstyle.directive';
+
+describe('HoverstyleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HoverstyleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/hoverstyle.directive.ts
@@ -0,0 +1,31 @@
+import {Directive, ElementRef, HostListener, Input, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[appHoverstyle]'
+})
+export class HoverstyleDirective implements OnInit {
+ @Input() txtDecorate: string;
+ @Input() fontWeight: string;
+  constructor(private elm: ElementRef) {
+    this.txtDecorate = "";
+    this.fontWeight = "";
+  }
+
+  ngOnInit(): void {
+    // this.elm.nativeElement.style.textDecoration = this.txtDecorate;
+    // this.elm.nativeElement.style.fontWeight = this.fontWeight;
+  }
+
+  @HostListener('mouseover') onHover(){
+    this.hoverMe(this.txtDecorate, this.fontWeight);
+  }
+
+  @HostListener('mouseout') onHoverOff(){
+    this.hoverMe("","");
+  }
+
+  private hoverMe(txtDecoration?: string, fntWeight?: string){
+    this.elm.nativeElement.style.textDecoration = txtDecoration;
+    this.elm.nativeElement.style.fontWeight = fntWeight;
+  }
+}
