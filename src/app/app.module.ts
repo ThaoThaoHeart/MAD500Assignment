9dbd98c3431e720b9c1874ccba5ef7b3407d7bd0
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-11-30 13:12:27 -0500

    Assignment10 Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
